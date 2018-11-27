9f52c5d5a1805a753a37fafedaf5ed389310abd8
gijith <gijithau@hotmail.com>
2018-11-27 20:16:42 +0530

    after the table changes

---
+++ dotnetcore-sample/Models/Customers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnetcore_sample.Models
+{
+    public partial class Customers
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string City { get; set; }
+        public string Status { get; set; }
+        public DateTime SysStartTime { get; set; }
+        public DateTime SysEndTime { get; set; }
+    }
+}
